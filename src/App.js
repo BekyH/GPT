84fe5fc8741a708b21b0c3b83c28ce792fdf5f20
BekyH <bereketheramo25@gmail.com>
2022-12-28 08:36:36 +0300

    initalized add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import Article from './components/article/Article'
+import Brand from './components/brand/Brand'
+import Cta from './components/cta/Cta'
+import Feature from './components/feature/Feature'
+import Navbar from './components/navbar/Navbar'
+ const App = () => {
+    return (
+        <div>
+         <h1>GPT-3</h1>   
+        </div>
+    )
 }
 
-export default App;
+export default App
